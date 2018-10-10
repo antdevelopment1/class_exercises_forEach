77000677f67aba7a1efdb2ca0bbcc76c5b6149eb
April Copes <antdevelopment1@gmail.com>
2018-10-09 21:49:02 -0400

    Creates a function that returns the square value of numbers passed

--- index.js
+++ index.js
@@ -36,3 +36,16 @@ function getPositive(list) {
     return positiveNumber;
 }
 console.log(getPositive([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]));
+
+// Write a function which takes an array of numbers as input and returns a new array containing result of 
+// squaring each of the numbers in the given array by two. Example: squareTheNumbers([1, 2, 3]) should give 
+// [1, 4, 9].
+
+function squared(list) {
+    let newList = [];
+    list.forEach(function(num) {
+        newList.push(num * num);
+    })
+    return newList;
+}
+console.log(squared([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
