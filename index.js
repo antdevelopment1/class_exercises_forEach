04a645032d023fe48839acfd76f7782e4f64d42e
April Copes <antdevelopment1@gmail.com>
2018-10-09 22:22:10 -0400

    Creates a function that returns an array of the names of the cities

--- index.js
+++ index.js
@@ -52,20 +52,36 @@
 
 // Write a function which takes an array of city objects like such:
 // as input and returns a new array containing the cities whose temperature is cooler than 70 degrees.
-function cooler(cities) {
-  let count = 0;
+// function cooler(cities) {
+//   let count = 0;
+//   cities.forEach(function(city) {
+//     if (cities[count]["temperature"] < 70) {
+//       console.log(city);
+//       count++;
+//     }
+//   })
+// }
+// cooler([
+//     { name: 'Los Angeles', temperature: 60.0},
+//     { name: 'Atlanta', temperature: 52.0 },
+//     { name: 'Detroit', temperature: 48.0 },
+//     { name: 'New York', temperature: 80.0 }
+// ]);
+
+// Write a function which takes an array of city objects like the above problem as input and returns 
+// an array of the cities names.
+function nameOfCities(cities) {
+  let counter = 0;
+  let list = [];
   cities.forEach(function(city) {
-    if (cities[count]["temperature"] < 70) {
-      console.log(city);
-      count++;
-    }
+    list.push(cities[counter]["name"]);
+    counter++;
   })
+  return list;
 }
-cooler([
+console.log(nameOfCities([
     { name: 'Los Angeles', temperature: 60.0},
     { name: 'Atlanta', temperature: 52.0 },
     { name: 'Detroit', temperature: 48.0 },
     { name: 'New York', temperature: 80.0 }
-]);
-
-  
+]));
